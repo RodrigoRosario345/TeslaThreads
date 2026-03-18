4169d1dfd2394a8589531eec1d8fc626e33e6975
RodrigoRosario345 <rodrigorosario345@gmail.com>
2026-03-18 00:13:30 -0400

    feat: refactor CartEmpty component layout for improved responsiveness and styling

--- src/components/cart/CartEmpty/CartEmpty.tsx
+++ src/components/cart/CartEmpty/CartEmpty.tsx
@@ -5,11 +5,11 @@ export function CartEmpty() {
     return (
         <div className="space-y-6 sm:px-12 mt-30 md:mt-12">
             <p className="text-xl text-center md:text-left text-gray-600">Your cart is empty.</p>
-            <div className="flex flex-col sm:flex-row gap-4">
-                <Link href="/" className="w-full sm:max-w-80">
+            <div className="flex flex-wrap gap-4">
+                <Link href="/" className="w-60 grow md:max-w-80">
                     <Button buttonStyle="primary" className="w-full px-0!">Continue Shopping</Button>
                 </Link>
-                <Link href="/" className="w-full sm:max-w-80">
+                <Link href="/" className="w-60 grow md:max-w-80">
                     <Button buttonStyle="borderDark" className="w-full px-0!">Sign In</Button>
                 </Link>
             </div>
