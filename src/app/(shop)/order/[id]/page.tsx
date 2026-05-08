01696037a2958a4512a52c8fe4f5cf8e8042bf02
RodrigoRosario345 <rodrigorosario435@gmail.com>
2026-05-08 13:25:22 -0400

    feat: implement order management features including order creation, retrieval, and not found handling

---
+++ src/app/(shop)/order/[id]/page.tsx
@@ -0,0 +1,40 @@
+import { getOrderById } from "@/actions/order";
+import { Metadata, NextPage } from "next";
+import { notFound } from "next/navigation";
+
+// export async function generateMetadata({ params }: Props): Promise<Metadata> {
+//     // read route params
+//     const { slug } = await params;
+
+//     const product = await getProductBySlug(slug);
+
+//     return {
+//         title: (product?.title || 'Product Not Found') + ' | Tesla Threads',
+//         description: product?.description || 'No description available.',
+//         openGraph: {
+//             title: product?.title || 'Product Not Found',
+//             description: product?.description || 'No description available.',
+//             images: `/products/${product?.images[0] || 'default-image.jpg'}`,
+//         },
+//     }
+// }
+interface Props {
+    params: Promise<{ id: string }>;
+}
+
+const OrderPage: NextPage<Props> = async ({ params }) => {
+    const { id } = await params;
+
+    const orderResult = await getOrderById(id);
+    if (!orderResult.order || !orderResult.success) {
+        notFound();
+    }
+
+    return (
+        <div>
+            {JSON.stringify(orderResult.order, null, 2)}
+        </div>
+    )
+};
+
+export default OrderPage;
--- src/components/checkout/CheckoutOrderSummary/CheckoutOrderSummary.tsx
+++ src/components/checkout/CheckoutOrderSummary/CheckoutOrderSummary.tsx
@@ -53,7 +53,6 @@ export function CheckoutOrderSummary({
                 status: "error",
                 message: result.message,
             });
-            return;
         } else {
             addOperationResult({
                 status: "success",
@@ -63,51 +62,47 @@ export function CheckoutOrderSummary({
         }
 
         router.replace("/cart");
-
     };
 
 
     return (
-        <>
-            <div className="w-full h-max space-y-3 lg:p-8 lg:rounded-md lg:shadow-xl lg:max-w-sm">
-                <h2 className="text-base md:text-lg font-bold">
-                    {`Order Summary${totalItems ? ` (${totalItems} items)` : ""}`}
-                </h2>
-                <AddressSummary />
-                <div className="w-full border-b-[0.5px] border-gray-300" />
-                <p className="flex justify-between items-center text-sm text-gray-600">
-                    <span>Subtotal</span>
-                    <span>{formatPrice(subtotal)}</span>
-                </p>
-                <p className="flex justify-between items-center text-sm text-gray-600">
-                    <span>Shipping</span>
-                    <span>{formatPrice(shipping)}</span>
-                </p>
-                <p className="flex justify-between items-center text-sm text-gray-600">
-                    <span>Sales Tax</span>
-                    <span>{formatPrice(tax)}</span>
-                </p>
-                <p className="flex justify-between items-center text-base md:text-lg font-bold">
-                    <span>Total</span>
-                    <span>{formatPrice(total)}</span>
-                </p>
-                <div className="w-full fixed bottom-0 left-0 px-4 py-6 text-center shadow-[0_-2px_10px_rgba(0,0,0,0.1)] bg-white lg:static lg:shadow-none lg:p-0">
-                    <Button
-                        type="button"
-                        variant={isSubmitting ? "primaryDisabled" : "primary"}
-                        className="w-full max-w-120"
-                        onClick={handlePlaceOrder}
-                        disabled={isSubmitting}
-                    >
-                        <LoadingText
-                            text="Place Order"
-                            isLoading={isSubmitting}
-                            loadingText="Processing..."
-                        />
-                    </Button>
-                </div>
+        <div className="w-full h-max space-y-3 lg:p-8 lg:rounded-md lg:shadow-xl lg:max-w-sm">
+            <h2 className="text-base md:text-lg font-bold">
+                {`Order Summary${totalItems ? ` (${totalItems} items)` : ""}`}
+            </h2>
+            <AddressSummary />
+            <div className="w-full border-b-[0.5px] border-gray-300" />
+            <p className="flex justify-between items-center text-sm text-gray-600">
+                <span>Subtotal</span>
+                <span>{formatPrice(subtotal)}</span>
+            </p>
+            <p className="flex justify-between items-center text-sm text-gray-600">
+                <span>Shipping</span>
+                <span>{formatPrice(shipping)}</span>
+            </p>
+            <p className="flex justify-between items-center text-sm text-gray-600">
+                <span>Sales Tax</span>
+                <span>{formatPrice(tax)}</span>
+            </p>
+            <p className="flex justify-between items-center text-base md:text-lg font-bold">
+                <span>Total</span>
+                <span>{formatPrice(total)}</span>
+            </p>
+            <div className="w-full fixed bottom-0 left-0 px-4 py-6 text-center shadow-[0_-2px_10px_rgba(0,0,0,0.1)] bg-white lg:static lg:shadow-none lg:p-0">
+                <Button
+                    type="button"
+                    variant={isSubmitting ? "primaryDisabled" : "primary"}
+                    className="w-full max-w-120"
+                    onClick={handlePlaceOrder}
+                    disabled={isSubmitting}
+                >
+                    <LoadingText
+                        text="Place Order"
+                        isLoading={isSubmitting}
+                        loadingText="Processing..."
+                    />
+                </Button>
             </div>
-
-        </>
+        </div>
     );
 }
