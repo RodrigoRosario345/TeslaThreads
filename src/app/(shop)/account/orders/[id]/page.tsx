ec6e2db37efde43e7c049e71c925291def03fba6
RodrigoRosario345 <rodrigorosario435@gmail.com>
2026-05-18 18:27:26 -0400

    refactor: remove unused metadata generation code and simplify title handling in product page

--- src/app/(shop)/account/orders/[id]/page.tsx
+++ src/app/(shop)/account/orders/[id]/page.tsx
@@ -1,24 +1,8 @@
 
 import { getOrderById } from "@/actions";
-import { Metadata, NextPage } from "next";
+import { NextPage } from "next";
 import { notFound } from "next/navigation";
 
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//     // read route params
-//     const { slug } = await params;
-
-//     const product = await getProductBySlug(slug);
-
-//     return {
-//         title: (product?.title || 'Product Not Found') + ' | Tesla Threads',
-//         description: product?.description || 'No description available.',
-//         openGraph: {
-//             title: product?.title || 'Product Not Found',
-//             description: product?.description || 'No description available.',
-//             images: `/products/${product?.images[0] || 'default-image.jpg'}`,
-//         },
-//     }
-// }
 interface Props {
     params: Promise<{ id: string }>;
 }
